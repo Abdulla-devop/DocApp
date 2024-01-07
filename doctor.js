9dbe9ff835af34ce9534b7e309353dd6f4beeca1
Abdulla/ Full Stack Developer <142766611+Abdulla-devop@users.noreply.github.com>
2024-01-07 11:58:01 +0530

    Add files via upload

--- doctor.js
+++ doctor.js
@@ -1,26 +1,80 @@
-import { ObjectId } from "bson";
-import {client} from "../db.js";
+import express from "express"
+import { addNewDoctor, deleteDoctor, editDoctor, getAllDoctors } from "../Controllers/doctor.js";
+import { isAuthorized } from "../Auth/auth.js";
 
-export async function getAllDoctors(){
-return client.db("Docapp")
-.collection("doctors")
-.find({})
-.toArray();
-}
- export function addNewDoctor(data){
-    return client.db("Docapp").collection("doctors").insertOne(data);
- }
+// initializing router
+const router = express.Router();
 
-export function editDoctor(id,data){
-    return client
-    .db("Docapp")
-    .collection("doctors")
-    .updateOne({_id:new ObjectId(id)},{$set:data});
-}
+// get all doctor data
+router.get("/all",async(req,res)=>{
+    try {
+         // get data from db
+    const doctors = await getAllDoctors();
+    // error handle if no data
+    if(doctors.length <= 0){
+        return res.status(404).json({message:"No data available"});
+    }
+    return res.status(200).json({data:doctors})
+    } catch (error) {
+         res
+         .status(500)
+         .json({error:"Internal server error",errorMessage: error});
+    }
+});
 
-export function deleteDoctor(id){
-    return client
-    .db("Docapp")
-    .collection("doctors")
-    .deleteOne({_id:new ObjectId(id)});
-}
+router.post("/add",isAuthorized, async(req,res)=>{
+    // we need to handle the error in req.body
+    try {
+        if(Object.keys(req.body).length <= 0){
+            return res.status(400).json({error:"Check request body"});
+        }
+        //    doctor data
+        const data = {... req.body, status:"Available"};
+        // adding data to db
+        const newDoctor = await addNewDoctor(data);
+        if(!newDoctor.acknowledged){
+            return res.status(400).json({error:"Error in adding data"})
+        }
+        res.status(201).json({data:newDoctor})
+    } catch (error) {
+        res
+        .status(500)
+        .json({error:"Internal server error",errorMessage: error});
+    }
+    });
+router.put("/edit/:id",async(req,res) => {
+    try {
+        const {id} = req.params;
+        if(Object.keys(req.body).length <= 0){
+            return res.status(400).json({error:"Check request body"}); 
+        }
+        const newlyUpdatedDoctor = await editDoctor(id,req.body);
+        if(!newlyUpdatedDoctor.acknowledged){
+            return res.status(400).json({error:"Error in updating data"})
+        }
+        res.status(201).json({data:newlyUpdatedDoctor})
+    } catch (error) {
+        res
+        .status(500)
+        .json({error:"Internal server error",errorMessage: error});
+    }
+});
+
+router.delete("/delete/:id",async(req,res) =>{
+    try {
+        const {id} = req.params;
+        // deleting a doctor
+        const doctorToDelete = await deleteDoctor(id);
+        if(!doctorToDelete.acknowledged){
+            return res.status(400).json({error:"error in deleting data"}); 
+        }
+        res.status(200).json({data:doctorToDelete})
+        
+    } catch (error) {
+        res
+        .status(500)
+        .json({error:"Internal server error",errorMessage: error});
+    }
+})
+
+    export const doctorRouter = router;
--- user.js
+++ user.js
@@ -1,15 +1,60 @@
-import { client } from "../db.js";
+import express from "express"
+import bcrypt from "bcrypt"
+import { getUser, registerUser } from "../Controllers/user.js";
+import { generateToken } from "../Auth/auth.js";
+const router = express.Router();
 
-export function registerUser(data){
-    return client
-    .db("Docapp")
-    .collection("users")
-    .insertOne(data)
-}
+//singup
 
-export function getUser(userEmail){
-    return client
-    .db("Docapp")
-    .collection("users")
-    .findOne({email:userEmail});
-}
+router.post("/signup",async(req,res) => {
+    try {
+        if(Object.keys(req.body).length <= 0){
+            return res.status(400).json({error:"Invalid request"});
+        }
+        // generate salt value
+        const salt = await bcrypt.genSalt(10);
+        // is the user already registerd
+        const checkUser =  await getUser(req.body.email);
+        if(!checkUser){
+            const hashedPass = await bcrypt.hash(req.body.password, salt);
+            const encryptUser = await {...req.body, password:hashedPass};
+        const user = await registerUser(encryptUser);
+        if(!user.acknowledged){
+            return res.status(400).json({error:"Failed Registration"});
+        }
+     return res.status(201).json({message:"Registered Sucessfuly",data:user})
+    }
+    res.status(400).json({error:"User Already Exist"});
+ } catch (error) {
+        res
+        .status(500)
+        .json({error:"Internal server error",errorMessage: error});
+    }
+});
+// login
+router.post("/login",async(req,res) => {
+    try {
+        if(Object.keys(req.body).length <= 0){
+            return res.status(400).json({error:"Invalid request"});
+    }
+    const checkUser =  await getUser(req.body.email);
+    if(!checkUser){
+         return res.status(404).json({error:"Invalid Email"});
+    }
+    const validPassword = await bcrypt.compare(
+        req.body.password,
+        checkUser.password
+    );
+    if(!validPassword){
+        return res.status(400).json({error:"Invalid password"});
+    }
+    const token = generateToken(checkUser._id);
+    res.status(200).json({message:"Logged in successfully",token})
+} catch (error) {
+        res
+        .status(500)
+        .json({error:"Internal server error",errorMessage: error});
+    }
+})
+
+export const userRouter = router;
